8d41e5d7d537fc9d3e659af751324d9d88417014
kxrweng <kwlimlearns@gmail.com>
2023-12-08 11:16:38 +0800

    VersaCaseStudy

---
+++ bigbang.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+let numArr = [];
+for (let i = 1; i <= 100; i++) {
+    numArr[i - 1] = i;
+}
+
+numArr = numArr.map((i) => {
+    return (i % 3 == 0 && i % 5 == 0) ? "BIGBANG" : (i % 3 == 0) ? "BIG" : (i % 5 == 0) ? "BANG" : i;
+});
+
+const jsonString = JSON.stringify(numArr, null, 2);
+fs.writeFileSync('output.json', jsonString);
+
