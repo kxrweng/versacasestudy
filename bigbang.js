a501443d5111b5f19df1391a9594d7b1caf5482e
KWLim <117881314+kxrweng@users.noreply.github.com>
2023-12-08 11:29:23 +0800

    Update bigbang.js

--- bigbang.js
+++ bigbang.js
@@ -8,6 +8,8 @@ numArr = numArr.map((i) => {
     return (i % 3 == 0 && i % 5 == 0) ? "BIGBANG" : (i % 3 == 0) ? "BIG" : (i % 5 == 0) ? "BANG" : i;
 });
 
-const jsonString = JSON.stringify(numArr, null, 2);
+const jsonString = JSON.stringify(numArr);
 fs.writeFileSync('output.json', jsonString);
+
+console.log(numArr);
 
